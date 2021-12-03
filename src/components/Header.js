3ea85ed00c3fbd3e5cd1f4c413bc311397f2b401
Caedan <caedanlavender@gmail.com>
2021-12-03 21:02:03 +1300

    Set up basics of navigation bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,43 @@
 const Header = () => {
-
+   const navigationLinks = [
+      {
+         title: "home",
+         link: '#'
+      },
+      {
+         title: "cat",
+         link: '#'
+      },
+      {
+         title: "dog",
+         link: '#'
+      },
+      {
+         title: "horse",
+         link: '#'
+      },
+      {
+         title: "rodent",
+         link: '#'
+      },
+      {
+         title: "reptile",
+         link: '#'
+      },
+      {
+         title: "fish",
+         link: '#'
+      },
+   ]
+   
    return (
-      <div>Header content goes here</div>
+      <div><ul>
+         {
+            navigationLinks.map((item) => (
+               <li>{item.title}</li>
+            ))
+         }
+      </ul></div>
    )
 }
 
