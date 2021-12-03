91edcdece67b58d52b66d9387d2e4fe71d04ab10
Caedan <caedanlavender@gmail.com>
2021-12-04 08:34:30 +1300

    Completed header component
    For now

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,15 @@
+import '../styles/Header.css'
+import { useState } from 'react'
+import { ReactComponent as PetLogo } from '../assets/pet-logo.svg'
+import { ReactComponent as AccountLogo } from '../assets/account.svg'
+import { ReactComponent as CartLogo } from '../assets/cart.svg'
+
+
 const Header = () => {
+   const [bannerIsOpen, setBannerIsOpen] = useState(true)
+
+   const toggleBanner = () => setBannerIsOpen(!bannerIsOpen);
+
    const navigationLinks = [
       {
          title: "home",
@@ -29,15 +40,36 @@ const Header = () => {
          link: '#'
       },
    ]
-   
+
    return (
-      <div><ul>
-         {
-            navigationLinks.map((item) => (
-               <li>{item.title}</li>
-            ))
-         }
-      </ul></div>
+      <header>
+         <div className={`callToActionSuper ${bannerIsOpen || 'gone'}`}>
+            <div className="callToAction">
+               we're open for all your pet needs | <span className='bold'>more covid-19 info here</span>
+               <div className='dismiss' onClick={toggleBanner}>x</div>
+            </div>
+         </div>
+         <div className="headerInner">
+            <nav>
+               <aside>
+                  <div className='logo'>
+                     <PetLogo />
+                  </div>
+               </aside>
+               <ul>
+                  {
+                     navigationLinks.map((item) => (
+                        <li>{item.title}</li>
+                        ))
+                     }
+               </ul>
+               <aside>
+                     <AccountLogo />
+                     <CartLogo />
+               </aside>
+            </nav>
+         </div>
+      </header>
    )
 }
 
