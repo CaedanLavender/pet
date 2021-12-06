f10726975291764dbd1ac459b3b12fa33670c541
Caedan <caedanlavender@gmail.com>
2021-12-07 07:16:32 +1300

    Added animal to house conversion function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,19 @@
 export const toSentenceCase = (word) => {
    return word[0].toUpperCase() + word.substr(1, word.length - 1)
 }
+
+export const animalHouse = (animal) => {
+   let house;
+   switch (animal.toLowerCase()) {
+      case 'dog':
+         house = 'The Doge House'
+         break;
+      case 'cat':
+         house = 'The Cat Botique'
+         break;
+      default:
+         house = animal;
+         break;
+   }
+   return house
+}
